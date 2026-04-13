6db326c5453370cb56b1bfc0777f28f713d366bd
efakmansoy <efakmansoy@gmail.com>
2026-04-13 23:57:28 +0300

    v2.4

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ Sentry.init({
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    void navigator.serviceWorker.register('/sw.js')
+    void navigator.serviceWorker.register(import.meta.env.BASE_URL + 'sw.js')
   })
 }
 
